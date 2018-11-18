577b4be0907da7c47b35132e8b2227762235ac0c
muffinman148 <pandamcg@gmail.com>
2018-11-17 16:33:29 -0800

    User Class - Start of php class User.

--- cgi-bin/user.php
+++ cgi-bin/user.php
@@ -1,10 +1,8 @@
 <?php
 class User {
     private $db;
-    private $username;
-    private $firstname;
-    private $lastname;
-    private $userrole = 0;
+    private $username;             // Emp_Username
+    private $userrole = 0;         // Emp_Authority
     private $userloggedin = false;
     
     function __construct($db) 
@@ -19,17 +17,51 @@ class User {
 
     function login()
     {
-        // Check Database
-        // User Logged in
-        // User not Logged in
+        if( isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+
+            $db = new Db();
+            $name = $db -> quote($_POST['username']);
+            // $pass = $db -> quote($_POST['password']);
+
+            $user = $db -> query("SELECT Emp_Name FROM Employee WHERE Emp_Name = ". $name . ";");
+            $hash = $db -> query("SELECT Emp_Password FROM Employee WHERE Emp_Name = ". $name . ";");
+            if(password_verify($pass, $hash)) {
+                return false;
+            }
+
+            if($_POST['username'] == $user && $_POST['password'] == $pass) {
+                $_SESSION['valid'] = true;
+                $_SESSION['timeout'] = time();
+                $_SESSION['username'] = 'tutorialspoint';
+            }
+
+
+            if($hash) {
+            } else {
+
+            }
+
+            // Check Database
+            // User Logged in
+            // User not Logged in
+        }
     }
     function logout()
     {
         // Log user out
+        session_start();
+        unset($_SESSION["username"]);
+        unset($_SESSION["password"]);
+
+        header('Refresh: 2; URL = login.html');
     }
     function register()
     {
+        $db = new Db();    
+        $name = $db -> quote($_POST['username']);
+        
         // Create new User
+        $result = $db -> query("INSERT INTO `users` (`name`,`email`) VALUES (" . $name . "," . $email . ")");
     }
     function setPassword()
     {
