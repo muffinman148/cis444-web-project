51a4010f2ecaa8179a68d9ab8b51b5f83eaa27b9
muffinman148 <pandamcg@gmail.com>
2018-12-02 11:03:43 -0800

    Removes hardcoded javascript redirect.

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,10 +3,10 @@ function validate()
 	var username = document.getElementById("username").value;
 	var pass = document.getElementById("password").value;
 	//Check username and password. Assume values a a are a valid user.
-	if((username=="a")&&(pass=="a"))
-		location.href="managerHome.html";
-	else
-		alert("Invalid username or password");
+	// if((username=="a")&&(pass=="a"))
+	//     location.href="managerHome.html";
+	// else
+	//     alert("Invalid username or password");
 
     if(username=="") { alert("Username field is empty. Please input a valid username."); }
     if(pass=="") { alert("Password field is empty. Please input a valid password."); }
